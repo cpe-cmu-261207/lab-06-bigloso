c0e6656aa02c0906dc2f305edf69829ee8af52e4
bigloso <59277363+bigloso@users.noreply.github.com>
2021-08-28 13:36:28 +0700

    About me

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+const About =() =>{
+    return(
+      <div className='text-center space-y-3'>
+      <p className='text-2xl font-semibold'>About me</p>
+      <p className='text-xl'>Nitipong Saengla 620612154</p>
+    </div>
+    )
+}
+
+export default About;
