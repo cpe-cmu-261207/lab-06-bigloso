2280005b2facdbad265da38b5e2d5811054ba3c3
bigloso <59277363+bigloso@users.noreply.github.com>
2021-08-28 13:37:36 +0700

    second

--- src/components/Current.tsx
+++ src/components/Current.tsx
@@ -1,1 +1,61 @@
+import axios from "axios"
+import { useEffect,useState } from "react"
+
+
+type Datatype={
+   time:{
+       updated:string;
+   };
+   bpi:{
+        THB:{
+            rate_float:number;
+        };
+   };
+
+}
+const Current = () =>{
+    const [current,setCurrent]=useState<Datatype|null>(null)
+    const [loading,setLoading] = useState<boolean>(true);
  
+    const fetchapi = async ()=>{
+        try{
+                const resp = await axios.get<Datatype>('https://api.coindesk.com/v1/bpi/currentprice/thb.json')
+                setCurrent(resp.data)
+                setLoading(false)
+        }
+        catch(err){
+                console.log(err)
+                setLoading(false)
+        }
+    }
+
+    useEffect(()=>{
+        fetchapi()
+    },[])
+
+    const render = () =>{
+        if(loading){
+            return (
+                <div className='text-center space-y-3'>
+                <p className='text-2xl font-semibold'>Current price</p>
+                <p className='text-2xl'>Loading ...</p>
+                </div>
+            )
+        }
+        else{
+            return(
+              <div className='text-center space-y-3'>
+                 <p className='text-2xl font-semibold'>Current price</p>
+                 <p className='text-2xl'>{(current?.bpi.THB.rate_float)?.toLocaleString()} THB</p>
+                  <p> (Last updated {current?.time.updated}) </p>
+              </div>  
+            )
+        }
+    }
+
+    return (
+       render()
+    )
+}
+
+export default Current
